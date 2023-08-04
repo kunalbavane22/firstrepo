414fa546d4a46f49c479c35ef76273bc0f5cf759
kunalbavane22 <141391055+kunalbavane22@users.noreply.github.com>
2023-08-04 12:27:54 +0530

    Update by dev 2 Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 public Static void main(String args[])
 {
-System.ou.println("Hello this is dev 1")
+System.ou.println("Hello this is dev 1");
+  System.ou.println("Hello this is dev 2");
 }
 }
