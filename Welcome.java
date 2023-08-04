61b4604031907b779b5b049cacf08a0129143260
kunalbavane22 <141391055+kunalbavane22@users.noreply.github.com>
2023-08-04 12:49:26 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -2,6 +2,7 @@ public class Welcome
 {
 public Static void main(String args[])
 {
-System.out.println("welcome to CJC classes")
+System.out.println("welcome to CJC classes");
+ System.out.println("welcome to Adhyayan classes"); 
 }
 }
